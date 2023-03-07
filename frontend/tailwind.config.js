43f6b5073b7086f86a7159d4ace0ab2f45c36efd
Johann <johann.dumoulin@asdoria.com>
2023-03-07 11:46:51 +0100

    feat: add fonts roboto and hennigar

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                core: ['Roboto', 'sans-serif'],
+                heading: ['Hennigar', 'sans-serif'],
+            }
+        }
+    }
+}
