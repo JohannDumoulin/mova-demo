874633536cdb733278172639df311c6e0d671922
Johann <johann.dumoulin@asdoria.com>
2023-03-07 08:44:24 +0100

    feat: add header component

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+<!--    <NuxtWelcome />-->
+
+      <Header/>
   </div>
 </template>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="bg-slate-500 p-6 flex justify-between text-white">
+        <img src="~assets/logo_white.svg" alt="">
+
+        <div class="flex gap-8">
+            <nav>
+                <ul class="flex gap-8">
+                    <li><NuxtLink to="/brewery">Brewery</NuxtLink></li>
+                    <li><NuxtLink to="/restaurant">Restaurant</NuxtLink></li>
+                    <li><NuxtLink to="/shop">Shop</NuxtLink></li>
+                    <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+                </ul>
+            </nav>
+
+            <p>0</p>
+
+            <div>Menu</div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
